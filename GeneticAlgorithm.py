a4202f452648eb63d07406b11d131a540bcf615d
larsQue11 <tommee.stone@gmail.com>
2019-11-23 21:17:24 -0500

    Initial commit

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,11 @@
+# https://www.youtube.com/watch?v=nrKjSeoc7fc
+# https://stackoverflow.com/questions/44497352/printing-colors-on-screen-in-python
+# https://stackoverflow.com/questions/12187354/get-rgb-value-opencv-python
+
+#Load target image and store the RGB data
+
+#Set up genetic information
+
+#Determine fitness criteria.
+    #Percent correct
+    #
